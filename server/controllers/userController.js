cf6984ebcc91b16836f284f113f5d26704c93d7d
Yethu Kichu <123456789+odin@users.noreply.github.com>
2022-10-23 21:52:40 +0530

    update

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,20 +1,25 @@
 const User = require("../model/user")
-
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+const KEY ="loveu";
 const signup = async(req,res,next)=>{
     const{name,email,password} = req.body
     let existingUser;
     try {
-        existingUser=User.findOne({email:email})
+        existingUser=await User.findOne({email:email})
     } catch (error) {
         console.log(error)
     }
     if(existingUser){
+        console.log(email)
         return res.status(400).json({message:"user already existed"});
+        
     }
+   const hashedPass = bcrypt.hashSync(password)
     const user = new User({
         name,
         email,
-        password,
+        password:hashedPass
     })
     try{
         await user.save();
@@ -23,4 +28,46 @@ const signup = async(req,res,next)=>{
     }
     return res.status(201).json({message:user});
 }
+
+
+
+const login=async(req,res,next)=>{
+    const {email,password} = req.body;
+    let existingUser;
+    try{
+    existingUser = await User.findOne({email:email})
+    }catch(e){
+      
+    }
+    if(!existingUser){
+        return res.status(400).json({message:"user not existed"});
+    }
+        const isPasswordCorrect = bcrypt.compareSync(password,existingUser.password)
+    
+    if(!isPasswordCorrect){
+        return res.status(400).json({message:"password is incorrect"});
+    }
+    const token = jwt.sign({id:existingUser._id},KEY,{
+        expiresIn:"1hr"
+    })
+    return res.status(200).json({message:"successfull",user:existingUser,token});
+
+   
+};
+   const verifiedToken=(req,res,next)=>{
+    const headers = req.headers[`authorization`];
+    const token = headers.split(" ")[1];
+    if(!token){
+        res.status(404).json({message:"no token found"})
+    }
+    jwt.verify(String(token),KEY,(err,user)=>{
+        if(err){
+         return   res.status(404).json({message:"invalid tokenfound"})
+
+        }
+        console.log(user.id)
+    })
+}
 exports.signup=signup;
+exports.login=login;
+exports.verifiedToken=verifiedToken;
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,11 @@
 const express= require("express");
-const { signup } = require("../controllers/userController");
+const { signup, login, verifiedToken } = require("../controllers/userController");
 const router = express.Router();
 
 router.post("/signup",signup);
+router.post("/login",login);
+router.get("/user",verifiedToken);
+
+
 
 module.exports=router;
