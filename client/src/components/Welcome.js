774a23ed5a971549d5c2ba881be29a6d95fd6332
Yethu Kichu <123456789+odin@users.noreply.github.com>
2022-10-25 12:28:40 +0530

    token time updated

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -3,17 +3,19 @@ import axios from 'axios'
 import { useEffect } from 'react';
 import {useState }from 'react'
 axios.defaults.withCredentials=true
-// let firstRender = true;
+let firstRender = true;
 function Welcome() {
   const [users, setusers] = useState()
-  //  const refreshToken=async()=>{
-  //   const res = await axios.get('http://localhost:5000/api/refresh',{
-  //     withCredentials:true
-  // }).catch((e)=>console.log(e))
+
+
+   const refreshToken=async()=>{
+    const res = await axios.get('http://localhost:5000/api/refresh',{
+      withCredentials:true
+  }).catch((err)=>console.log(err))
    
-  //  const data = await res.data
-  //  return data;
-  //  }
+   const data = await res.data 
+   return data;
+   }
  const sendRequest=async()=>{
     const res = await axios.get('http://localhost:5000/api/user',{
         withCredentials:true
@@ -23,14 +25,15 @@ function Welcome() {
      return data;
  }
 useEffect(() => {
-  //  if(firstRender){
-    // firstRender=false
+   if(firstRender){
+    firstRender=false
     sendRequest().then((data)=>setusers(data.user))
-  //  }
-  //  let intervel=setInterval(()=>{
-  //   refreshToken().then((data)=>setusers(data.user))
-  //  },1000*29)
-  // return ()=>clearInterval(intervel)
+   }
+   let intervel=setInterval(()=>{
+    refreshToken().then((data)=>setusers(data.user))
+   },1000*29)
+   
+  return ()=>clearInterval(intervel)
 }, [])
 
   return (
